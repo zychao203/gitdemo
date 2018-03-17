2e23e09a6571326d85943c096b9d93c493d17906
朱永超 <zhuyongchao@zhuyongchaodeMacBook-Pro.local>
2018-03-17 11:01:03 +0800

    Initial commit

---
+++ src/main/java/com/cloud127/demo/GitdemoApplication.java
@@ -0,0 +1,12 @@
+package com.cloud127.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitdemoApplication.class, args);
+	}
+}
